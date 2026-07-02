1155bcc1ec8f73252f9d75ffef0163257aa57ccc
alternativq <157893580+alternativq@users.noreply.github.com>
2026-07-02 15:42:04 +0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// Скопируйте js/config.example.js сюда и укажите данные Supabase
+window.SUPABASE_CONFIG = {
+  url: "https://rakzrscrnpquywqizjxh.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJha3pyc2NybnBxdXl3cWl6anhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5MTUwNTEsImV4cCI6MjA5ODQ5MTA1MX0.wcFUUhvALvY8p2ojUROa6yq9GF5wQEQ8V8MVHfAqQRY",
+};
