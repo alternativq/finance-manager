3c0aaf25006502780efe589b809eba7dd6d53635
alternativq <danek4959@gmail.com>
2026-07-02 14:50:49 +0300

    fix bd

--- js/config.example.js
+++ js/config.example.js
@@ -1,5 +1,5 @@
 // Скопируйте этот файл как js/config.js и укажите данные вашего Supabase-проекта
 window.SUPABASE_CONFIG = {
-  url: "https://your-project.supabase.co",
-  anonKey: "your-anon-key",
+  url: "https://rakzrscrnpquywqizjxh.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJha3pyc2NybnBxdXl3cWl6anhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5MTUwNTEsImV4cCI6MjA5ODQ5MTA1MX0.wcFUUhvALvY8p2ojUROa6yq9GF5wQEQ8V8MVHfAqQRY",
 };
